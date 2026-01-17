c47ebf7df7c6f4e7a659c7fb8071426ef829bc80
nhilalyildirim <63622425+nhilalyildirim@users.noreply.github.com>
2026-01-17 23:31:37 +0300

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,1 +1,13 @@
-/// <reference types="vite/client" />
+// Replaces /// <reference types="vite/client" /> to avoid "Cannot find type definition file" error
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  [key: string]: any;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module 'mammoth';
+declare module 'pdfjs-dist';
